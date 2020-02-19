5541ec272f7d2861a6842819a9e419a8a351ef8b
sonny <wb-szq493230@alibaba-inc.com>
2020-02-19 11:11:12 +0800

    add java oss util

---
+++ util/java/src/main/java/com/aliyun/ossutil/models/RuntimeOptions.java
@@ -0,0 +1,60 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ossutil.models;
+
+import com.aliyun.tea.*;
+
+public class RuntimeOptions extends TeaModel {
+    @NameInMap("autoretry")
+    public boolean autoretry;
+
+    @NameInMap("ignoreSSL")
+    public boolean ignoreSSL;
+
+    @NameInMap("maxAttempts")
+    public Integer maxAttempts;
+
+    @NameInMap("backoffPolicy")
+    public String backoffPolicy;
+
+    @NameInMap("backoffPeriod")
+    public Integer backoffPeriod;
+
+    @NameInMap("readTimeout")
+    public Integer readTimeout;
+
+    @NameInMap("connectTimeout")
+    public Integer connectTimeout;
+
+    @NameInMap("localAddr")
+    public String localAddr;
+
+    @NameInMap("httpProxy")
+    public String httpProxy;
+
+    @NameInMap("httpsProxy")
+    public String httpsProxy;
+
+    @NameInMap("noProxy")
+    public String noProxy;
+
+    @NameInMap("maxIdleConns")
+    public Integer maxIdleConns;
+
+    @NameInMap("socks5Proxy")
+    public String socks5Proxy;
+
+    @NameInMap("socks5NetWork")
+    public String socks5NetWork;
+
+    @NameInMap("uploadLimitSpeed")
+    public Integer uploadLimitSpeed;
+
+    @NameInMap("listener")
+    public Object listener;
+
+    public static RuntimeOptions build(java.util.Map<String, ?> map) throws Exception {
+        RuntimeOptions self = new RuntimeOptions();
+        return TeaModel.build(map, self);
+    }
+
+}
---
+++ util/java/src/test/java/com/aliyun/ossutil/GetBucketResponse.java
@@ -0,0 +1,104 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ossutil;
+
+import com.aliyun.tea.NameInMap;
+import com.aliyun.tea.TeaModel;
+import com.aliyun.tea.Validation;
+
+public class GetBucketResponse extends TeaModel {
+    @NameInMap("x-oss-request-id")
+    @Validation(required = true)
+    public String requestId;
+
+    @NameInMap("ListBucketResult")
+    @Validation(required = true)
+    public GetBucketResponseListBucketResult listBucketResult;
+
+    @NameInMap("body")
+    @Validation()
+    public Body body;
+
+    public static class Body extends TeaModel {
+        @NameInMap("body")
+        @Validation(required = true)
+        public GetBucketResponseListBucketResult contents;
+    }
+
+    public static class GetBucketResponseListBucketResultContentsOwner extends TeaModel {
+        @NameInMap("ID")
+        @Validation(required = true)
+        public String iD;
+
+        @NameInMap("DisplayName")
+        @Validation(required = true)
+        public String displayName;
+
+    }
+
+    public static class GetBucketResponseListBucketResultContents extends TeaModel {
+        @NameInMap("Key")
+        @Validation(required = true)
+        public String key;
+
+        @NameInMap("ETag")
+        @Validation(required = true)
+        public String eTag;
+
+        @NameInMap("LastModified")
+        @Validation(required = true)
+        public String lastModified;
+
+        @NameInMap("Size")
+        @Validation(required = true)
+        public String size;
+
+        @NameInMap("StorageClass")
+        @Validation(required = true)
+        public String storageClass;
+
+        @NameInMap("Owner")
+        @Validation(required = true)
+        public GetBucketResponseListBucketResultContentsOwner owner;
+
+    }
+
+    public static class GetBucketResponseListBucketResult extends TeaModel {
+        @NameInMap("Name")
+        @Validation(required = true)
+        public String name;
+
+        @NameInMap("Prefix")
+        @Validation(required = true)
+        public String prefix;
+
+        @NameInMap("Marker")
+        @Validation(required = true)
+        public String marker;
+
+        @NameInMap("MaxKeys")
+        @Validation(required = true)
+        public String maxKeys;
+
+        @NameInMap("Delimiter")
+        @Validation(required = true)
+        public String delimiter;
+
+        @NameInMap("IsTruncated")
+        @Validation(required = true)
+        public String isTruncated;
+
+        @NameInMap("EncodingType")
+        @Validation(required = true)
+        public String encodingType;
+
+        @NameInMap("CommonPrefixes")
+        @Validation(required = true)
+        public String commonPrefixes;
+
+        @NameInMap("Contents")
+        @Validation(required = true)
+        public GetBucketResponseListBucketResultContents[] contents;
+
+    }
+
+}
