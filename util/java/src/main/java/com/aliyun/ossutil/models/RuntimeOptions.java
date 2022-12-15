c1656463148f25b24ee10b5a9eade9179e86d98d
nanhe <papaya.dyn@alibaba-inc.com>
2022-12-15 15:07:07 +0800

    rebuild RuntimeOptions model

--- util/java/src/main/java/com/aliyun/ossutil/models/RuntimeOptions.java
+++ util/java/src/main/java/com/aliyun/ossutil/models/RuntimeOptions.java
@@ -3,58 +3,237 @@ package com.aliyun.ossutil.models;
 
 import com.aliyun.tea.*;
 
+/**
+ * The OSS runtime options model
+ */
 public class RuntimeOptions extends TeaModel {
+    /**
+     * <p>autoretry</p>
+     */
     @NameInMap("autoretry")
     public boolean autoretry;
 
+    /**
+     * <p>ignoreSSL</p>
+     */
     @NameInMap("ignoreSSL")
     public boolean ignoreSSL;
 
+    /**
+     * <p>max attempts</p>
+     */
     @NameInMap("maxAttempts")
     public Integer maxAttempts;
 
+    /**
+     * <p>backoff policy</p>
+     */
     @NameInMap("backoffPolicy")
     public String backoffPolicy;
 
+    /**
+     * <p>backoff period</p>
+     */
     @NameInMap("backoffPeriod")
     public Integer backoffPeriod;
 
+    /**
+     * <p>read timeout</p>
+     */
     @NameInMap("readTimeout")
     public Integer readTimeout;
 
+    /**
+     * <p>connect timeout</p>
+     */
     @NameInMap("connectTimeout")
     public Integer connectTimeout;
 
+    /**
+     * <p>local addr</p>
+     */
     @NameInMap("localAddr")
     public String localAddr;
 
+    /**
+     * <p>http proxy</p>
+     */
     @NameInMap("httpProxy")
     public String httpProxy;
 
+    /**
+     * <p>https proxy</p>
+     */
     @NameInMap("httpsProxy")
     public String httpsProxy;
 
+    /**
+     * <p>no proxy</p>
+     */
     @NameInMap("noProxy")
     public String noProxy;
 
+    /**
+     * <p>max idle conns</p>
+     */
     @NameInMap("maxIdleConns")
     public Integer maxIdleConns;
 
+    /**
+     * <p>socks5 proxy</p>
+     */
     @NameInMap("socks5Proxy")
     public String socks5Proxy;
 
+    /**
+     * <p>socks5 NetWork</p>
+     */
     @NameInMap("socks5NetWork")
     public String socks5NetWork;
 
+    /**
+     * <p>upload limit speed</p>
+     */
     @NameInMap("uploadLimitSpeed")
     public Integer uploadLimitSpeed;
 
+    /**
+     * <p>listener</p>
+     */
     @NameInMap("listener")
     public Object listener;
 
     public static RuntimeOptions build(java.util.Map<String, ?> map) {
         RuntimeOptions self = new RuntimeOptions();
         return TeaModel.build(map, self);
+    }
+
+    public RuntimeOptions setAutoretry(boolean autoretry) {
+        this.autoretry = autoretry;
+        return this;
+    }
+    public boolean getAutoretry() {
+        return this.autoretry;
+    }
+
+    public RuntimeOptions setIgnoreSSL(boolean ignoreSSL) {
+        this.ignoreSSL = ignoreSSL;
+        return this;
+    }
+    public boolean getIgnoreSSL() {
+        return this.ignoreSSL;
+    }
+
+    public RuntimeOptions setMaxAttempts(Integer maxAttempts) {
+        this.maxAttempts = maxAttempts;
+        return this;
+    }
+    public Integer getMaxAttempts() {
+        return this.maxAttempts;
+    }
+
+    public RuntimeOptions setBackoffPolicy(String backoffPolicy) {
+        this.backoffPolicy = backoffPolicy;
+        return this;
+    }
+    public String getBackoffPolicy() {
+        return this.backoffPolicy;
+    }
+
+    public RuntimeOptions setBackoffPeriod(Integer backoffPeriod) {
+        this.backoffPeriod = backoffPeriod;
+        return this;
+    }
+    public Integer getBackoffPeriod() {
+        return this.backoffPeriod;
+    }
+
+    public RuntimeOptions setReadTimeout(Integer readTimeout) {
+        this.readTimeout = readTimeout;
+        return this;
+    }
+    public Integer getReadTimeout() {
+        return this.readTimeout;
+    }
+
+    public RuntimeOptions setConnectTimeout(Integer connectTimeout) {
+        this.connectTimeout = connectTimeout;
+        return this;
+    }
+    public Integer getConnectTimeout() {
+        return this.connectTimeout;
+    }
+
+    public RuntimeOptions setLocalAddr(String localAddr) {
+        this.localAddr = localAddr;
+        return this;
+    }
+    public String getLocalAddr() {
+        return this.localAddr;
+    }
+
+    public RuntimeOptions setHttpProxy(String httpProxy) {
+        this.httpProxy = httpProxy;
+        return this;
+    }
+    public String getHttpProxy() {
+        return this.httpProxy;
+    }
+
+    public RuntimeOptions setHttpsProxy(String httpsProxy) {
+        this.httpsProxy = httpsProxy;
+        return this;
+    }
+    public String getHttpsProxy() {
+        return this.httpsProxy;
+    }
+
+    public RuntimeOptions setNoProxy(String noProxy) {
+        this.noProxy = noProxy;
+        return this;
+    }
+    public String getNoProxy() {
+        return this.noProxy;
+    }
+
+    public RuntimeOptions setMaxIdleConns(Integer maxIdleConns) {
+        this.maxIdleConns = maxIdleConns;
+        return this;
+    }
+    public Integer getMaxIdleConns() {
+        return this.maxIdleConns;
+    }
+
+    public RuntimeOptions setSocks5Proxy(String socks5Proxy) {
+        this.socks5Proxy = socks5Proxy;
+        return this;
+    }
+    public String getSocks5Proxy() {
+        return this.socks5Proxy;
+    }
+
+    public RuntimeOptions setSocks5NetWork(String socks5NetWork) {
+        this.socks5NetWork = socks5NetWork;
+        return this;
+    }
+    public String getSocks5NetWork() {
+        return this.socks5NetWork;
+    }
+
+    public RuntimeOptions setUploadLimitSpeed(Integer uploadLimitSpeed) {
+        this.uploadLimitSpeed = uploadLimitSpeed;
+        return this;
+    }
+    public Integer getUploadLimitSpeed() {
+        return this.uploadLimitSpeed;
+    }
+
+    public RuntimeOptions setListener(Object listener) {
+        this.listener = listener;
+        return this;
+    }
+    public Object getListener() {
+        return this.listener;
     }
 
 }
--- util/php/src/OSSUtils/RuntimeOptions.php
+++ util/php/src/OSSUtils/RuntimeOptions.php
@@ -1,44 +1,239 @@
 <?php
 
 // This file is auto-generated, don't edit it. Thanks.
-
 namespace AlibabaCloud\Tea\OSSUtils\OSSUtils;
 
 use AlibabaCloud\Tea\Model;
 
+/**
+ * The OSS runtime options model
+ */
 class RuntimeOptions extends Model
 {
+    protected $_name = [
+        'autoretry' => 'autoretry',
+        'ignoreSSL' => 'ignoreSSL',
+        'maxAttempts' => 'maxAttempts',
+        'backoffPolicy' => 'backoffPolicy',
+        'backoffPeriod' => 'backoffPeriod',
+        'readTimeout' => 'readTimeout',
+        'connectTimeout' => 'connectTimeout',
+        'localAddr' => 'localAddr',
+        'httpProxy' => 'httpProxy',
+        'httpsProxy' => 'httpsProxy',
+        'noProxy' => 'noProxy',
+        'maxIdleConns' => 'maxIdleConns',
+        'socks5Proxy' => 'socks5Proxy',
+        'socks5NetWork' => 'socks5NetWork',
+        'uploadLimitSpeed' => 'uploadLimitSpeed',
+        'listener' => 'listener',
+    ];
+    public function validate()
+    {
+    }
+    public function toMap()
+    {
+        $res = [];
+        if (null !== $this->autoretry) {
+            $res['autoretry'] = $this->autoretry;
+        }
+        if (null !== $this->ignoreSSL) {
+            $res['ignoreSSL'] = $this->ignoreSSL;
+        }
+        if (null !== $this->maxAttempts) {
+            $res['maxAttempts'] = $this->maxAttempts;
+        }
+        if (null !== $this->backoffPolicy) {
+            $res['backoffPolicy'] = $this->backoffPolicy;
+        }
+        if (null !== $this->backoffPeriod) {
+            $res['backoffPeriod'] = $this->backoffPeriod;
+        }
+        if (null !== $this->readTimeout) {
+            $res['readTimeout'] = $this->readTimeout;
+        }
+        if (null !== $this->connectTimeout) {
+            $res['connectTimeout'] = $this->connectTimeout;
+        }
+        if (null !== $this->localAddr) {
+            $res['localAddr'] = $this->localAddr;
+        }
+        if (null !== $this->httpProxy) {
+            $res['httpProxy'] = $this->httpProxy;
+        }
+        if (null !== $this->httpsProxy) {
+            $res['httpsProxy'] = $this->httpsProxy;
+        }
+        if (null !== $this->noProxy) {
+            $res['noProxy'] = $this->noProxy;
+        }
+        if (null !== $this->maxIdleConns) {
+            $res['maxIdleConns'] = $this->maxIdleConns;
+        }
+        if (null !== $this->socks5Proxy) {
+            $res['socks5Proxy'] = $this->socks5Proxy;
+        }
+        if (null !== $this->socks5NetWork) {
+            $res['socks5NetWork'] = $this->socks5NetWork;
+        }
+        if (null !== $this->uploadLimitSpeed) {
+            $res['uploadLimitSpeed'] = $this->uploadLimitSpeed;
+        }
+        if (null !== $this->listener) {
+            $res['listener'] = $this->listener;
+        }
+        return $res;
+    }
+    /**
+     * @param array $map
+     * @return RuntimeOptions
+     */
+    public static function fromMap($map = [])
+    {
+        $model = new self();
+        if (isset($map['autoretry'])) {
+            $model->autoretry = $map['autoretry'];
+        }
+        if (isset($map['ignoreSSL'])) {
+            $model->ignoreSSL = $map['ignoreSSL'];
+        }
+        if (isset($map['maxAttempts'])) {
+            $model->maxAttempts = $map['maxAttempts'];
+        }
+        if (isset($map['backoffPolicy'])) {
+            $model->backoffPolicy = $map['backoffPolicy'];
+        }
+        if (isset($map['backoffPeriod'])) {
+            $model->backoffPeriod = $map['backoffPeriod'];
+        }
+        if (isset($map['readTimeout'])) {
+            $model->readTimeout = $map['readTimeout'];
+        }
+        if (isset($map['connectTimeout'])) {
+            $model->connectTimeout = $map['connectTimeout'];
+        }
+        if (isset($map['localAddr'])) {
+            $model->localAddr = $map['localAddr'];
+        }
+        if (isset($map['httpProxy'])) {
+            $model->httpProxy = $map['httpProxy'];
+        }
+        if (isset($map['httpsProxy'])) {
+            $model->httpsProxy = $map['httpsProxy'];
+        }
+        if (isset($map['noProxy'])) {
+            $model->noProxy = $map['noProxy'];
+        }
+        if (isset($map['maxIdleConns'])) {
+            $model->maxIdleConns = $map['maxIdleConns'];
+        }
+        if (isset($map['socks5Proxy'])) {
+            $model->socks5Proxy = $map['socks5Proxy'];
+        }
+        if (isset($map['socks5NetWork'])) {
+            $model->socks5NetWork = $map['socks5NetWork'];
+        }
+        if (isset($map['uploadLimitSpeed'])) {
+            $model->uploadLimitSpeed = $map['uploadLimitSpeed'];
+        }
+        if (isset($map['listener'])) {
+            $model->listener = $map['listener'];
+        }
+        return $model;
+    }
+    /**
+     * @description autoretry
+     * @var bool
+     */
     public $autoretry;
 
+    /**
+     * @description ignoreSSL
+     * @var bool
+     */
     public $ignoreSSL;
 
+    /**
+     * @description max attempts
+     * @var int
+     */
     public $maxAttempts;
 
+    /**
+     * @description backoff policy
+     * @var string
+     */
     public $backoffPolicy;
 
+    /**
+     * @description backoff period
+     * @var int
+     */
     public $backoffPeriod;
 
+    /**
+     * @description read timeout
+     * @var int
+     */
     public $readTimeout;
 
+    /**
+     * @description connect timeout
+     * @var int
+     */
     public $connectTimeout;
 
+    /**
+     * @description local addr
+     * @var string
+     */
     public $localAddr;
 
+    /**
+     * @description http proxy
+     * @var string
+     */
     public $httpProxy;
 
+    /**
+     * @description https proxy
+     * @var string
+     */
     public $httpsProxy;
 
+    /**
+     * @description no proxy
+     * @var string
+     */
     public $noProxy;
 
+    /**
+     * @description max idle conns
+     * @var int
+     */
     public $maxIdleConns;
 
+    /**
+     * @description socks5 proxy
+     * @var string
+     */
     public $socks5Proxy;
 
+    /**
+     * @description socks5 NetWork
+     * @var string
+     */
     public $socks5NetWork;
 
+    /**
+     * @description upload limit speed
+     * @var int
+     */
     public $uploadLimitSpeed;
 
+    /**
+     * @description listener
+     * @var mixed
+     */
     public $listener;
-
-    protected $_name = [];
 }
